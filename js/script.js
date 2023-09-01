0f44cdf5ffc241792dfcc5bf6ff39ef4f85df9f0
cjenschke <cimarron13@mac.com>
2023-09-01 18:33:41 -0500

    need to find reason why highscore disappearing

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let score = 0;
 let timeLeft = 60;
 let timerInterval;
 let highScore = 0;
+let finalScore = score;
+
 
 // Elements
 const startButton = document.getElementById("start-btn");
@@ -11,14 +13,17 @@ const startOverBtn = document.getElementById('start-over-btn');
 const questionElement = document.getElementById("question");
 const choicesElement = document.getElementById("choices");
 const highScoreElement = document.getElementById("high-score");
-const scoreEl = document.getElementById('current-score');
+const scoreElement = document.getElementById('current-score');
 const h1Element = document.querySelector('h1');
 const messageElement = document.getElementById('message');
+let highScoreDisplay = localStorage.getItem('highScoreDisplay');
+let savedInitials = localStorage.getItem('initials');
+const timerDisplay = document.getElementById('current-time');
 
 const quizData = [
     {
         question: 'What is JavaScript used for?',
-        choices: ['Create dynamic content for websites.', 'Coffe label for customer orders.', 'To style and layout web pages.', 'To structure a web page and its content.'],
+        choices: ['Create dynamic content for websites.', 'Coffee label for customer orders.', 'To style and layout web pages.', 'To structure a web page and its content.'],
         correctAnswer: "Create dynamic content for websites."
     },
     {
@@ -35,7 +40,12 @@ const quizData = [
         question: 'Choose what is not an HTML element.',
         choices: ['class name', 'id', 'tag name', 'function'],
         correctAnswer: 'function'
-    }
+    },
+    {
+        question: 'Do you think this quiz was fun?',
+        choices: ['No', 'Yes', 'If I win.', 'Not if I lose.'],
+        corectAnswer: 'If I win.'
+    },
 ];
 
 // Functions
@@ -69,7 +79,6 @@ function selectAnswer(event) {
         h1Element.textContent = 'Correct!';
         updateScore(score);
     } else {
-        // messageElement.textContent = 'Wrong Answer!'
         h1Element.textContent = 'Wrong Answer!';
         subtractTime()
     }
@@ -84,25 +93,23 @@ function selectAnswer(event) {
 }
 
 function updateScore(score) {
-    scoreEl.textContent = score;
+    scoreElement.textContent = score;
 }
 
 function startTimer() {
-    // Update the timer display on the UI
-    const timerDisplay = document.getElementById('current-time');
-    timerDisplay.textContent = timeLeft;
-
-    // Start the countdown
-    const countdown = setInterval(function () {
+    timerInterval = setInterval(() => {
         timeLeft--;
         timerDisplay.textContent = timeLeft;
-
-        // Check if the time is up
-        if (timeLeft <= 0) {
-            clearInterval(countdown);
-            // Add code here to handle when the time is up
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            endQuiz();
         }
     }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
+    console.log('timer stopped');
 }
 
 function subtractTime() {
@@ -112,20 +119,33 @@ function subtractTime() {
     timerDisplay.textContent = timeLeft;
 }
 
+function resetGame() {
+    score = 0;
+    startQuiz();
+
+}
+
 function endQuiz() {
-    clearInterval(timerInterval);
+    stopTimer();
     h1Element.textContent = 'Game Over';
     document.getElementById("score-display").textContent = "Your score " + score;
+    let initials = prompt("Enter your initials:");
+    // let finalScore = score;
+    localStorage.setItem('initials', initials);
+    // localStorage.setItem('high score', 'highScore');
+    if (score > highScore) {
+        highScore = score;
+        localStorage.setItem('high score', highScore);
 
-    const initials = prompt("Enter your initials:");
-    const finalScore = score;
-
-    // if (finalScore > highScore) {
-    //     highScore = finalScore;
-    //     highScoreElement.textContent = `${initials}: ${highScore}`;
-    // }
+        // highScoreElement.textContent = 'Your high score is' + highScore;
+        highScoreElement.textContent = `${initials}: ${highScore}`;
+    }
 }
+
+
 
 // Event listeners
 startButton.addEventListener("click", startQuiz);
-startOverBtn.addEventListener("click", endQuiz);
+startOverBtn.addEventListener("click", function () {
+    window.location.reload()
+});
